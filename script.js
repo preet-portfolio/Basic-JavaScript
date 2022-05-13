523d4d42caa5ca7a6bcf36fffa886eeebd3e7969
Preet Panchal <panc0051@algonquinlive.com>
2022-05-13 16:08:21 -0400

    Main

---
+++ script.js
@@ -0,0 +1,11 @@
+//console.log("Page successfully loaded script.js");
+var website = "https://codingforeverybody.com/";
+var teacherName = "Kalob Taulien";
+
+console.log(website);
+console.log(teacherName);
+
+let apples = 6;
+const height = "5'10\"";
+// console.log("i have " + apples + " apples");
+//  console.log("my height is " + height);
